a60d19f923b1d14571196196ea730d15f71967b1
Stewie <62187379+Huy-To@users.noreply.github.com>
2026-06-11 11:23:49 -0400

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int current_count = 0;
+        int max_count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                current_count++;
+                if (current_count > max_count) {
+                    max_count = current_count;
+                }
+            }
+            else {
+                current_count = 0;
+            }
+        
+        }
+        return max_count;
+    }
+};
